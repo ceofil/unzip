3602acf8a865a19e94c4a0731b6f6d333c26d3a6
CiobanuTeofil <gceofil@gmail.com>
2019-03-28 19:42:24 +0200

    threading is working properly

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from math import *
 import random
 import queue
 
-N = 2  # number of threads
+N = 4  # number of threads
 nr_of_files = 5
 threads = []
 
@@ -17,48 +17,60 @@ class MyThread (threading.Thread):
         self.data_size = 0
         self.occupied = False
         self.time_stamp = time.time()
+        self.exit_flag = False
+        self.has_just_been_occupied = False
 
     def extract_file(self): # to simulate extracting a big file until I implement that
         time.sleep(self.data_size)
+        self.occupied = False
 
     def occupy(self, data_size):
         print("Thread {} was occupied with {}".format(self.thread_id, data_size))
         self.occupied = True
+        self.has_just_been_occupied = True
         self.time_stamp = time.time()
         self.data_size = data_size
 
     def run(self):
-        self.extract_file()
-        self.occupied = False
-        print("Thread {} was freed ".format(self.thread_id))
-
+        while not self.exit_flag:
+            if self.has_just_been_occupied:
+                self.has_just_been_occupied = False
+                self.extract_file()
+        print("Thread {} has finished".format(self.thread_id))
+            
 
 files = queue.Queue()
-suma = 0
+sum_of_times = 0
 
 start = time.time()
 
 # add some "files" to the queue
 for i in range(nr_of_files):
-    size = random.randint(1, 3)
+    size = random.randint(1, 10)
     files.put(size)
-    suma += size
+    sum_of_times += size
     print(size)
 
 # initialize threads
 for i in range(N):
     thr = MyThread(i)
     threads.append(thr)
+    thr.start()
 
 
 while not files.empty():
     for thread_it in threads:
         if (not files.empty()) and (not thread_it.occupied):
             thread_it.occupy(files.get())
-            thread_it.start()
+
+for thread_to_kill in threads:
+    thread_to_kill.exit_flag = True
+
+for thread_to_kill in threads:
+    thread_to_kill.join()
 
 end = time.time()
 print('Exiting Main Thread')
-print(floor(end-start))
-print(suma)
+print(floor(end-start))  # if threading is working correctly this should be lower than sum_of_times
+print(sum_of_times)
 

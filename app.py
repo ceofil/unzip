536992ab4ef43ca80db2b8b1bb1d72a5311c5a40
CiobanuTeofil <gceofil@gmail.com>
2019-03-29 14:00:59 +0200

    logging
    small files are extracted faster with fewer threads

--- app.py
+++ app.py
@@ -5,16 +5,17 @@ from math import *
 import queue
 import zipfile
 import os
+import logging
 
-
+logging.basicConfig(filename ='test.log', level=logging.INFO)
 directory = 'C:\\Users\\Teofil\\Desktop\\directory'
 output_dir = 'C:\\Users\\Teofil\\Desktop\\directory\\Fantasy'
 
 files = queue.Queue()
-sum_of_times = 0
-N = 4  # number of threads
+N = 1  # number of threads
 nr_of_files = 0
 nr_files_extracted = 0
+sum_of_file_size = 0
 threads = []
 
 
@@ -26,12 +27,17 @@ class MyThread (threading.Thread):
         self.time_stamp = time.time()
         self.exit_flag = False
         self.has_just_been_occupied = False
-        self.has_just_finished = False # update_console will be called only if this is true
+        self.has_just_finished = False  # update_console will be called only if this is true
         self.file_to_extract = None
+
+    def get_elapsed_time(self):
+        return floor(time.time() - self.time_stamp)
+
+    def get_file_size(self):
+        return sum(zinfo.file_size for zinfo in self.file_to_extract.filelist)
 
     def extract_file(self):
         self.file_to_extract.extractall(output_dir)
-        self.file_to_extract.close()
         self.occupied = False
         self.has_just_finished = True
 
@@ -54,7 +60,7 @@ def update_console():
     for thr in threads:
         print("Thread {}".format(thr.thread_id), end=' ')
         if thr.occupied:
-            print("is extracting {}".format(thr.file_to_extract.filename))
+            print("is extracting {} of size {}".format(thr.file_to_extract.filename, thr.get_file_size()))
         elif files.empty():
             print('is FREE but the queue is empty ')
         else:
@@ -64,9 +70,17 @@ def update_console():
 
 def every_thread_has_finished():
     for thr in threads:
-        if thr.occupied:
+        if thr.occupied or thr.has_just_finished:
             return False
     return True
+
+
+def log_thread_info(thr):
+    logging.info("file {}; duration {}; size {};".format(\
+        thread_it.file_to_extract.filename,\
+        thread_it.get_elapsed_time(),\
+        thread_it.get_file_size()))
+
 
 
 start = time.time()
@@ -90,6 +104,9 @@ while not files.empty():
             thread_it.has_just_finished = False
             nr_files_extracted += 1
             update_console()
+            log_thread_info(thread_it)
+            sum_of_file_size += thread_it.get_file_size()
+            thread_it.file_to_extract.close()
         if (not files.empty()) and (not thread_it.occupied):
             thread_it.occupy(files.get())
             update_console()
@@ -100,6 +117,10 @@ while not every_thread_has_finished():
             thread_it.has_just_finished = False
             nr_files_extracted += 1
             update_console()
+            log_thread_info(thread_it)
+            sum_of_file_size += thread_it.get_file_size()
+            if not thread_it.file_to_extract.fp:  
+                thread_it.file_to_extract.close()
 
 
 update_console()
@@ -110,5 +131,6 @@ for thread_to_kill in threads:
     thread_to_kill.join()
 
 end = time.time()
-elapsed_time = end - start
+elapsed_time = floor(end - start)
 print(" Elapsed time: {}".format(elapsed_time))
+logging.info("extracted {} of data in {}s with {} threads".format(sum_of_file_size, elapsed_time, N))

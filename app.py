e6fc15a99192f2861ab633f087adc800b385985c
CiobanuTeofil <gceofil@gmail.com>
2019-03-29 10:14:18 +0200

    update_console()
    updating the console only after a file has been extracted

--- app.py
+++ app.py
@@ -4,9 +4,14 @@ import time
 from math import *
 import random
 import queue
+import zipfile
+import os
 
+files = queue.Queue()
+sum_of_times = 0
 N = 4  # number of threads
-nr_of_files = 5
+nr_of_files = 12
+nr_files_extracted = 0
 threads = []
 
 
@@ -19,13 +24,15 @@ class MyThread (threading.Thread):
         self.time_stamp = time.time()
         self.exit_flag = False
         self.has_just_been_occupied = False
+        self.has_just_finished = False # update_console will be called only if this is true
+        self.file = None
 
     def extract_file(self): # to simulate extracting a big file until I implement that
         time.sleep(self.data_size)
         self.occupied = False
+        self.has_just_finished = True
 
     def occupy(self, data_size):
-        print("Thread {} was occupied with {}".format(self.thread_id, data_size))
         self.occupied = True
         self.has_just_been_occupied = True
         self.time_stamp = time.time()
@@ -36,17 +43,33 @@ class MyThread (threading.Thread):
             if self.has_just_been_occupied:
                 self.has_just_been_occupied = False
                 self.extract_file()
-        print("Thread {} has finished".format(self.thread_id))
-            
 
-files = queue.Queue()
-sum_of_times = 0
+
+def update_console():
+    os.system('cls')
+    print(" {} / {} files extracted".format(nr_files_extracted, nr_of_files))
+    for thr in threads:
+        print("Thread {}".format(thr.thread_id), end=' ')
+        if thr.occupied:
+            print("is extracting a file of size {}".format(thr.data_size))
+        elif files.empty():
+            print('is FREE but the queue is empty ')
+        else:
+            print('is FREE ')
+
+
+def every_thread_has_finished():
+    for thr in threads:
+        if thr.occupied:
+            return False
+    return True
+
 
 start = time.time()
 
 # add some "files" to the queue
 for i in range(nr_of_files):
-    size = random.randint(1, 10)
+    size = random.randint(1, 5)
     files.put(size)
     sum_of_times += size
     print(size)
@@ -62,7 +85,21 @@ while not files.empty():
     for thread_it in threads:
         if (not files.empty()) and (not thread_it.occupied):
             thread_it.occupy(files.get())
+            update_console()
+        elif thread_it.has_just_finished:
+            thread_it.has_just_finished = False
+            nr_files_extracted += 1
+            update_console()
 
+while not every_thread_has_finished():
+    for thread_it in threads:
+        if thread_it.has_just_finished:
+            thread_it.has_just_finished = False
+            nr_files_extracted += 1
+            update_console()
+
+
+update_console()
 for thread_to_kill in threads:
     thread_to_kill.exit_flag = True
 
@@ -70,7 +107,6 @@ for thread_to_kill in threads:
     thread_to_kill.join()
 
 end = time.time()
-print('Exiting Main Thread')
 print(floor(end-start))  # if threading is working correctly this should be lower than sum_of_times
 print(sum_of_times)
 
